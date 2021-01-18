652f61d5e8aad53d288d0b13432e2b0e3a12ff83
danieltruong555 <danieltruong555@gmail.com>
2021-01-17 23:03:34 -0800

    initial commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentService.Controllers
+{
+    [Authorize]
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (StudentDBEntities entities = new StudentDBEntities())
+            {
+                return entities.Students.ToList();
+            }
+        }
+    }
+}
